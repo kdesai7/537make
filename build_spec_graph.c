af7fda2574751982e59300955c7a3929b65e0fca
Mark Wiemer <markwiemer@outlook.com>
2018-11-04 13:13:48 -0600

    Gather names for spec graph

--- build_spec_graph.c
+++ build_spec_graph.c
@@ -5,7 +5,20 @@
 #include <stdlib.h>
 
 #include "build_spec_graph.h"
+#include "build_spec_repr.h"
 #include "node.h"
+
+/**
+ * Adds the given element to the end of the list if it isn't already in the list
+ * Assumes list is a non-null header node, element is non-null string
+ * Returns 0 on success, nonzero on failure
+ */
+int addIfNotIn(Node* list, char* element) {
+	if (indexOf(list, element) == -1) {
+		return append(list, (void*)element);
+	}
+	return 0;
+}
 
 /**
  * Builds a graph based on the given linked list of targets
@@ -14,6 +27,29 @@
  * Returns a reference to the graph on success
  */
 void* buildSpecGraph(Node* targetsHeader) {
-	// TODO
+	Node* names = newNode(NULL); // currently stored names
+	char* name; // current name to add
+	Node* targetNode = targetsHeader;
+	TargetInfo* target;
+	Node* dep;
+
+	if (names == NULL) return NULL;
+
+	// Add all the names of targets and their dependencies
+	while((targetNode = targetNode->next) != NULL) {
+		target = (TargetInfo*)targetNode->element;
+		name = target->name;
+		// Add target's name
+		addIfNotIn(names, name);
+		// Add target's dependencies
+		dep = target->deps;
+		while ((dep = dep->next) != NULL) {
+			name = (char*)dep->element;
+			addIfNotIn(names, name);
+		}
+	}
+
+	printStringNodes(names); // for debugging purposes TODO remove
+
 	return NULL;
 }
